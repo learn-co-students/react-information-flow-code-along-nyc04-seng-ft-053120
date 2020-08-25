9c9a3560388fbe8147f9ff25976e3b8cb83a10cb
BarbaraBontempo <barbarabontempo94@gmail.com>
2020-08-25 13:07:41 -0400

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -7,15 +7,26 @@ class Parent extends Component {
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: '#FFF'
     }
+  }
+  
+  changeColor = (newChildColor) => {
+    this.setState({
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+{/* passing down the parent state changing function (changeColor as a prop to the child components) */}
+{/* now the child will have this prop called handleColorChange */}
+{/* next step will be to pass this handleColorChange prop into event handler */}
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
